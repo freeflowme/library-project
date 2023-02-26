12ee7ecc9a83dc190a501b1b0f4469d610338dfd
freeflowme <116676605+freeflowme@users.noreply.github.com>
2023-02-25 19:39:42 -0600

    Fix intiial displayBook() logic

--- script.js
+++ script.js
@@ -9,7 +9,8 @@ function Book(title, author, pages, read) {
 }
 
 const newBookInfo = document.querySelector('#submit');
-    submit.addEventListener('click', () => {
+    submit.addEventListener('click', (e) => {
+        e.preventDefault();
         const newBook = new Book();
         newBook.title = document.querySelector('#title').value;
         newBook.author = document.querySelector('#author').value;
@@ -19,6 +20,7 @@ const newBookInfo = document.querySelector('#submit');
         if (library.includes(newBook) === false) {
             library.push(newBook);
             console.log(library);
+            displayBook();
         }
     }
 )
@@ -26,19 +28,26 @@ const newBookInfo = document.querySelector('#submit');
 function displayBook() {
     for (let Book of library) {
         const card = document.createElement('div');
+            card.className = 'card';
+            document.querySelector('.display').appendChild(card);
         const title = document.createElement('p');
             title.textContent = Book.title;
+            card.appendChild(title);
         const author = document.createElement('p');
             author.textContent = Book.author;
+            card.appendChild(author);
         const pages = document.createElement('p');
             pages.textContent = Book.pages;
+            card.appendChild(pages);
         const read = document.createElement('p');
-            read.textContent = Book.read; 
+            read.textContent = Book.read;
+            card.appendChild(read);
     }
 }
 
-
+/*
 const Hobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', 'read');
 const Fellowship = new Book('The Fellowship of the Ring', 'J.R.R. Tolkein', '423', 'read');
 const Towers = new Book('The Two Towers', 'J.R.R. Tolkein', '352', 'read');
 const King = new Book('The Return of the King', 'J.R.R. Tolkein', '416', 'read');
+*/
