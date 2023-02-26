b423b05aea39583cb0886f9ae0fd0f90cf2363c4
freeflowme <116676605+freeflowme@users.noreply.github.com>
2023-02-25 18:43:49 -0600

    Add initial display Book logic

--- script.js
+++ script.js
@@ -23,27 +23,22 @@ const newBookInfo = document.querySelector('#submit');
     }
 )
 
-
-/*
-addBookToLibrary();
-console.log(library);
-
 function displayBook() {
-    const bookCard = document.createElement('div');
-    const title = document.createElement('p');
-    const author = document.createElement('p');
-    const pages = document.createElement('p');
-    const read = document.createElement('p');
-}
-
-function displayLibrary() {
     for (let Book of library) {
-        displayBook(Book);
+        const card = document.createElement('div');
+        const title = document.createElement('p');
+            title.textContent = Book.title;
+        const author = document.createElement('p');
+            author.textContent = Book.author;
+        const pages = document.createElement('p');
+            pages.textContent = Book.pages;
+        const read = document.createElement('p');
+            read.textContent = Book.read; 
     }
 }
+
 
 const Hobbit = new Book('The Hobbit', 'J.R.R. Tolkein', '295', 'read');
 const Fellowship = new Book('The Fellowship of the Ring', 'J.R.R. Tolkein', '423', 'read');
 const Towers = new Book('The Two Towers', 'J.R.R. Tolkein', '352', 'read');
 const King = new Book('The Return of the King', 'J.R.R. Tolkein', '416', 'read');
-*/
